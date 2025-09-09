8066b3decda57a90aed4cc5e35e6a6e9863aab0f
kiritiaditya <kiriti17aditya@gmail.com>
2025-09-10 00:28:02 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,44 @@
-variable "env" {}
-variable "region" {}
-variable "vpc_cidr" {}
-variable "public_subnet_cidr" {}
-variable "az" {}
-variable "ami" {}
-variable "instance_type" {}
-variable "key_name" {}
-variable "db_user" {}
-variable "db_pass" {}
-variable "db_name" {}
+variable "env" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "public_subnet_cidrs" {
+  type = list(string)
+}
+
+variable "azs" {
+  type = list(string)
+}
+
+variable "ami" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "key_name" {
+  type = string
+}
+
+variable "db_user" {
+  type = string
+}
+
+variable "db_pass" {
+  type = string
+  sensitive = true
+}
+
+variable "db_name" {
+  type = string
+}
