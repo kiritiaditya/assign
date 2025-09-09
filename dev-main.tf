147be94a8cc07bd6d96358e9f3fec12943a47878
kiritiaditya <kiriti17aditya@gmail.com>
2025-09-09 22:04:46 +0530

    Create dev-main.tf

---
+++ dev-main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "vpc" {
+  source             = "../../modules/vpc"
+  env                = "dev"
+  vpc_cidr           = "10.0.0.0/16"
+  public_subnet_cidr = "10.0.1.0/24"
+  az                 = "us-east-1a"
+}
+
+module "ec2" {
+  source         = "../../modules/ec2"
+  env            = "dev"
+  ami            = "ami-04b4f1a9cf54c11d0" # Ubuntu
+  instance_type  = "t2.micro"
+  subnet_id      = module.vpc.public.id
+  key_name       = "ansible"
+}
+
+module "rds" {
+  source   = "../../modules/rds"
+  env      = "dev"
+  db_user  = "admin"
+  db_pass  = "Password123!"
+  db_name  = "mydb"
+  sg_id    = module.vpc.public.id
+}
